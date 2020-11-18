ef19f623a2e354a6641c37604f223fd5ea96b671
Jeslyn Mendonca <motipa@gmail.com>
2020-11-18 15:26:31 +0400

    navigation changes

---
+++ Vii/Vii/Views/SignUp.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Vii.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SignUp : ContentPage
+    {
+        public SignUp()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new HomePage());
+        }
+    }
+}

a44fadff9f3bb638a2fe2d4347265cb7c110d9ee
pooja nikam <poojag.nikam@gmail.com>
2020-12-05 12:03:48 +0400

    View

--- Vii/Vii.Android/MainActivity.cs
+++ Vii/Vii.Android/MainActivity.cs
@@ -25,7 +25,7 @@ namespace Vii.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            CarouselViewRenderer.Init();
+            //CarouselViewRenderer.Init();
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
